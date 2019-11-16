348091313bc6c6bc416170146dbed2b443c457fa
LukeVidalis <loukaslks@gmail.com>
2019-11-16 18:13:31 +0000

    Added array access counter and comparison counter

--- sketch.js
+++ sketch.js
@@ -5,7 +5,8 @@ let i = 0;
 let c_size = 0;
 let adjusted_width = window.innerWidth * 0.95;
 let adjusted_height = window.innerHeight * 0.95;
-
+let comparisons = 0;
+let array_acccesses = 0;
 function setup() {
     createCanvas(adjusted_width, adjusted_height);
     setup_ui();
@@ -14,7 +15,7 @@ function setup() {
 function setup_ui() {
     sizeSlider = createSlider(0, 1000, 200);
     sizeSlider.position(20, 20);
-    sizeSlider.style('width', '200px');
+    sizeSlider.style('width', '260px');
 
     button = createButton('Start');
     button.position(sizeSlider.x, 50);
@@ -28,7 +29,8 @@ function init_array() {
         values[i] = random(height);
     }
     c_size = adjusted_width / sizeSlider.value();
-
+    comparisons = 0;
+    array_acccesses =0;
     sorted_values = values.slice();
     sorted_values.sort((a, b) => a - b);
 }
@@ -39,26 +41,31 @@ function draw() {
     textSize(20);
     stroke(0);
     fill(255, 255, 255);
-    text(size, sizeSlider.x * 2 + sizeSlider.width, 30);
-    if(adjusted_width/c_size>450){
+    text("Number of Values: " + size, sizeSlider.x + button.width + 10, button.y+button.height/2);
+    text("Comparisons: " + comparisons, sizeSlider.x * 4 + sizeSlider.width, 30);
+    text("Array Accesses: " + array_acccesses, sizeSlider.x * 4 + sizeSlider.width, button.y+button.height/2);
+
+    if (adjusted_width / c_size > 450) {
         strokeWeight(0);
-    }
-    else{
+    } else {
         strokeWeight(1);
     }
     if (i < values.length) {
         for (let j = 0; j < values.length - i - 1; j++) {
             let a = values[j];
+            array_acccesses++;
             let b = values[j + 1];
+            array_acccesses++;
             if (a > b) {
                 swap(values, j, j + 1);
             }
-
+            comparisons++;
         }
     } else {
         //sort = false;
     }
     i++;
+
     for (let i = 0; i < values.length; i++) {
         rect(adjusted_width - i * c_size, height, c_size, values[i] - adjusted_height);
         stroke(255, 255, 255);
@@ -73,6 +80,9 @@ function draw() {
 
 function swap(arr, a, b) {
     let temp = arr[a];
+    array_acccesses++;
     arr[a] = arr[b];
+    array_acccesses++;
     arr[b] = temp;
+    array_acccesses++;
 }
